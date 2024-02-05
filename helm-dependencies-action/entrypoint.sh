c0f73b7b876a8eb91dc3bf9bdc875d3c52670956
Lucas Mari <lucasmari17@gmail.com>
2024-02-05 00:21:03 -0300

    Fix function

--- helm-dependencies-action/entrypoint.sh
+++ helm-dependencies-action/entrypoint.sh
@@ -28,7 +28,7 @@ function get_latest_artifacthub {
 
   # fetches latest version from rss feed (xml format)
   curl -sSL "https://artifacthub.io/api/v1/packages/helm/$HELM_NAME/feed/rss" | \
-    yq -p=xml '.rss.channel.item[0] > latest'
+    yq -p=xml '.rss.channel.item[0]' > latest
 }
 
 # global param: <PARAM_GIT_USER_EMAIL>
